b5ae803216932a35e4eb2929a83903342654b3ee
Paul <caliguidpaul@gmail.com>
2022-11-01 15:03:35 +0800

    working beta version

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ let operator = "";
 let num1 = null;
 let num2 = null;
 let operatorClickedBool = false;
+let editing = true;
+let keyClicked = "";
 
 buttonsArray.forEach(button => {
     if (button.classList.contains('number')) {
@@ -26,30 +28,55 @@ buttonsArray.forEach(button => {
     else if (button.id === "clear") {
         button.addEventListener('click', clearClicked);
     }
+
+    else if (button.id === "delete") {
+        button.addEventListener('click', deleteClicked);
+    }
+
+    else if (button.id === "dot") {
+        button.addEventListener('click', dotClicked);
+    }
 })
 
-function numberClicked(e) {
+function numberClicked(e, keyboardChar = null) {
+    editing = true;
+    let toAdd;
     if (currentString === "") {
         bigText.innerHTML = "";
     }
-
-    currentString += e.target.innerHTML;
-    bigText.innerHTML += e.target.innerHTML;
+    if(keyboardChar) {
+        toAdd = keyboardChar;
+    } else {
+        toAdd = e.target.innerHTML
+    }
+    currentString += toAdd;
+    bigText.innerHTML += toAdd;
 }
 
-function operatorClicked(e) {
-    if(operatorClickedBool === true) {
-        equalsClicked();
+function operatorClicked(e, keyboardChar = null) {
+    editing = false;
+    let toAdd;
+    if(keyboardChar) {
+        toAdd = keyboardChar;
+    } else {
+        toAdd = e.target.innerHTML
     }
-
-    num1 = Number(currentString);
-    operator = e.target.innerHTML;
-    smallText.innerHTML = (currentString + " " + operator + " ");
+    if(currentString) {
+        num1 = Number(currentString);
+    } else {
+        return;
+    }
+    operator = toAdd;
+    smallText.innerHTML = (num1 + " " + operator + " ");
     currentString = "";
     operatorClickedBool = true;
 }
 
 function equalsClicked(e) {
+    if(!operatorClickedBool) {
+        return;
+    }
+
     if (currentString === "") {
         num2 = null;
     } else {
@@ -65,6 +92,7 @@ function equalsClicked(e) {
     result = operate(operator, num1, num2);
     currentString = result;
     bigText.innerHTML = result;
+    editing = false;
     operatorClickedBool = false;
     operator = "";
 }
@@ -77,6 +105,66 @@ function clearClicked(e) {
     num1 = null;
     num2 = null;
     operatorClickedBool = false;
+}
+
+function deleteClicked(e) {     
+    if(editing && currentString !== ""){
+        bigText.innerHTML = bigText.innerHTML.slice(0,-1);
+        currentString = currentString.slice(0,-1);
+    }
+
+    else if(operatorClickedBool && currentString === "") {
+        smallText.innerHTML = smallText.innerHTML.slice(0,-2);
+        operatorClickedBool = false;
+    }
+}
+
+document.addEventListener('keydown', (event) => {
+    var code = event.code;
+    if (code === 'Digit0' || code === 'Numpad0') {
+        numberClicked(event, '0');
+    } else if (code === 'Digit1' || code === 'Numpad1') {
+        numberClicked(event, '1');
+    } else if (code === 'Digit2' || code === 'Numpad2') {
+        numberClicked(event, '2');
+    } else if (code === 'Digit3' || code === 'Numpad3') {
+        numberClicked(event, '3');
+    } else if (code === 'Digit4' || code === 'Numpad4') {
+        numberClicked(event, '4');
+    } else if (code === 'Digit5' || code === 'Numpad5') {
+        numberClicked(event, '5');
+    } else if (code === 'Digit6' || code === 'Numpad6') {
+        numberClicked(event, '6');
+    } else if (code === 'Digit7' || code === 'Numpad7') {
+        numberClicked(event, '7');
+    } else if (code === 'Digit8' || code === 'Numpad8') {
+        numberClicked(event, '8');
+    } else if (code === 'Digit9' || code === 'Numpad9') {
+        numberClicked(event, '9');
+    } else if (code === 'NumpadAdd') {
+        operatorClicked(event, '+');
+    } else if (code === 'NumpadSubtract') {
+        operatorClicked(event, '−');
+    } else if (code === 'NumpadMultiply') {
+        operatorClicked(event, '×');
+    } else if (code === 'NumpadDivide') {
+        operatorClicked(event, '÷');
+    } else if (code === 'NumpadEnter' || code === 'Enter') {
+        equalsClicked(event);
+    } else if (code === 'NumpadDecimal') {
+        dotClicked(event);
+    } else if (code === 'Backspace') {
+        deleteClicked(event);
+    } 
+    
+  }, false);
+
+function dotClicked(e) {
+    if (currentString.includes(".")) {
+        return;
+    }
+    bigText.innerHTML += ".";
+    currentString += ".";
 }
 
 function operate(operator, num1, num2) {
